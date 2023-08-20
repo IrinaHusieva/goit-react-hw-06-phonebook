599d155c76bb10931b00830f86c108d8388d04bf
IrinaHusieva <121337267+IrinaHusieva@users.noreply.github.com>
2023-08-20 19:57:12 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,72 @@
+import { useState, useEffect } from 'react';
+import { Section } from "./Section/Section";
+import { Filter } from "./Filter/Filter";
+import { nanoid } from 'nanoid';
+import { ContactForm } from "./ContactForm/ContactForm";
+import { ContactList } from "./ContactList/ContactList";
+
+
+
+export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+  
+
+  useEffect(() => {
+    const localData = localStorage.getItem('contacts');
+    const dataParse = JSON.parse(localData);
+    if (localData && dataParse.length > 0) {
+      setContacts(dataParse);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+
+  const addContact = (name, number) => {
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    if (findContact(name)) {
+      return alert(`${name} is already in contacts`);
+    }
+
+    setContacts((prevContacts) => (
+      [...prevContacts, newContact]));
+  }
+
+
+  const filterChange = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const filterContacts = () => contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()))
+
+  const findContact = name => contacts.find(contact => contact.name.toLowerCase() === name.toLowerCase());
+
+  const onDelete = (contactId) => {
+    setContacts((prevContacts) => prevContacts.filter((contact) => contact.id !== contactId));
+  };
+
+  const filteredContacts = filterContacts();
+
+  return (
+    <>
+      <Section title="Phonebook">
+        <ContactForm onSubmit={addContact} />
+      </Section>
+      <Section title="Contacts">
+        <Filter
+          filter={filter}
+          handleChange={filterChange}
+        />
+        <ContactList contacts={filteredContacts} onDelete={onDelete} />
+      </Section>
+    </>
+  );
+}
